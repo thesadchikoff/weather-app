9e6ee9e6279f84788b0d436727f9985a998f9e2d
bombasters <59049797+bombasters@users.noreply.github.com>
2023-02-19 14:51:05 +0300

    Add files via upload

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import { FiInfo } from 'react-icons/fi'
 const Loader = () => {
 	return (
-		<div className='flex desktop:items-center mobile:items-start mobile:p-[20px] desktop:flex-row mobile:flex-col mobile:gap-14 mobile:justify-center desktop:justify-center text-white desktop:p-10 desktop:rounded-xl  mobile:rounded-t-2xl text-center'>
-			<h1>Загрузка приложения. Ожидайте...</h1>
+		<div className='px-4'>
+			<div className='flex desktop:items-center mobile:items-center mobile:p-[20px] desktop:flex-row mobile:flex-row mobile:gap-3 mobile:justify-start desktop:justify-center text-white desktop:p-10 desktop:rounded-xl  mobile:rounded-2xl mt-5 mobile:w-full desktop:h-max mobile:bg-white mobile:text-black'>
+				<FiInfo className='text-blue-600 text-xl font-bold' />
+				<h1 className='font-semibold opacity-[0.6]'>Загрузка приложения...</h1>
+			</div>
 		</div>
 	)
 }
